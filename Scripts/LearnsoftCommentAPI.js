9703a3c3657610008a305f8c26c04bb198755c68
cuteo408 <lam@xixir.com>
2021-07-28 14:50:20 -0700

    fixed bugs

--- Scripts/LearnsoftCommentAPI.js
+++ Scripts/LearnsoftCommentAPI.js
@@ -24,8 +24,6 @@
             if (URL == null)
                 URL = document.getElementById(DataID).getAttribute("data-url");
 
-            //$('#' + DataID).html('<div id="dk-loading-div" style="position:fixed; z-index: 99999; left: 50%; top: 50%; transform: translate(-50%,-50%); -ms-transform: translate(-50%,-50%); margin:auto; text-align:center"><img src="/images/spinner-whitebg-grayfg.gif"></div>');
-
             comment_sdk.ws(URL, function (e) {
                 $('#' + DataID).html(e);
             });
@@ -35,11 +33,8 @@
             comment_sdk.ws(RemoveURL, null, null, function () { comment_sdk.load(DataID); });
         },
         count: function (obj, target) {
-             var max = obj.getAttribute("maxlength");
-            obj.value.length;
             $('#' + target).html(obj.value.length);
         }
-
 
     }
 })(0, 0);
